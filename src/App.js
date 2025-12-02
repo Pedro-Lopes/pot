fed4f2c6fe0e516cd545d4bc2e8b26b694eca610
Pedro Lopes <lopesjamaicapl@gmail.com>
2025-12-02 19:09:39 -0300

    teste 6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
 import './App.css';
-//teste 2 3 4 5 6
+//teste 2 3 4 5 6 7
 function App() {
   return (
     <div className="App">
